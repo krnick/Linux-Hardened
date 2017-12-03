e97ad152b89dc10630b2037ec65836e0184edca8
Your Name <you@example.com>
2017-12-03 14:39:21 +0800

    weq

--- useful_shell/track_setuid.sh
+++ useful_shell/track_setuid.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#find suid program
+
+
+mtime=7
+
+
+#find -perm  4000 up is setuid
+
+find / -type f  -perm +4000 -print0 |while read -d '' -r match
+
+do
+	if [ -x "$match" ] ; then
+	
+	owner="$(ls -ld $match|awk '{print $3}')"
+	perms="$(ls -ld $match|cut -c5-10|grep 'w')"
+
+	if [ ! -z $perms ] ;then
+		echo "*****$match is setuid owner is $owner"
+	fi
+	fi
+done
+
 
 
 
