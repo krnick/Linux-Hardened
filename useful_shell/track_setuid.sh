1bba8ad9f217bfacbeaa43ce8f3e472f7d193cc5
root <root@corpus.nuk.edu.tw>
2017-12-04 10:38:33 +0800

    add track setgid

--- useful_shell/track_setuid.sh
+++ useful_shell/track_setuid.sh
@@ -18,5 +18,21 @@ do
 done
 
 
+echo -e "\e[5m ****Tracking setgid**** \e[25m"
+
+find / -type f  -perm /2000 -xdev  -print0 |while read -d '' -r match
+
+do
+        if [ -x "$match" ] ; then
+
+        owner="$(ls -ld $match|awk '{print $3}')"
+        perms="$(ls -ld $match|cut -c5-10|grep 'w')"
+
+        if [ ! -z $perms ] ;then
+                echo -e " \e[1m \e[101m*****$match is setgid owner is $owner\e[0m"
+        fi
+        fi
+done
+
 
 
